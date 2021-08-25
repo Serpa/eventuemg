07fc964a8d2865a405f34e8a1772c88a8e9d8313
Guilherme Serpa Batista <serpa419@gmail.com>
2021-08-25 12:40:55 -0300

    web

--- views/coautores_projeto.php
+++
@@ -1,8 +0,0 @@
-<h1>Inserir novo Coautor</h1>
-<form method="post" action="processa_coautores.php">
-	<br>
-	<label class="badge badge-secondary">Nome do Coautor:</label><br>
-	<input class="form-control" type="text" name="nome_coautor" placeholder="Insira o nome do coautor" required>
-	<br>
-	<input type="submit" class="btn btn-success" value="Inserir Coautor">
-</form>
